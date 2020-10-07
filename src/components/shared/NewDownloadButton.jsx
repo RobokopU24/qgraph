061244672e1aecd0bba7e9f3a8ac3b8501a2aefb
Max Wang <max@covar.com>
2020-10-07 17:04:28 -0400

    Handle error when trying to download message data

--- src/components/shared/NewDownloadButton.jsx
+++ src/components/shared/NewDownloadButton.jsx
@@ -1,30 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useContext } from 'react';
 import Button from '@material-ui/core/Button';
 import DescriptionIcon from '@material-ui/icons/Description';
 import CircularProgress from '@material-ui/core/CircularProgress';
 
+import AlertContext from '@/context/alert';
+
 export default function NewDownloadButton({ displayText, getData, fileName }) {
   const [loading, setLoading] = useState(false);
+  const displayAlert = useContext(AlertContext);
 
   async function download() {
     setLoading(true);
 
     const response = await getData();
-    const blob = new Blob([JSON.stringify(response)], { type: 'application/json' });
-    const a = document.createElement('a');
-    a.download = fileName();
-    a.href = window.URL.createObjectURL(blob);
-    document.body.appendChild(a);
-    a.click();
-    a.remove();
+    if (response.status === 'error') {
+      displayAlert('error', response.message);
+    } else {
+      const blob = new Blob([JSON.stringify(response)], { type: 'application/json' });
+      const a = document.createElement('a');
+      a.download = fileName();
+      a.href = window.URL.createObjectURL(blob);
+      document.body.appendChild(a);
+      a.click();
+      a.remove();
+    }
 
     setLoading(false);
   }
 
   return (
     <Button
-      onClick={!loading && download}
-      startIcon={!loading && <DescriptionIcon />}
+      onClick={download}
+      startIcon={<DescriptionIcon />}
+      disabled={loading}
       variant="contained"
       size="large"
       color="secondary"
