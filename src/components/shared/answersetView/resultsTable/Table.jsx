337a5f79931609ee8093db44c5425d02db9910c2
Max Wang <max@covar.com>
2020-10-20 12:11:58 -0400

    Remove unused useState

--- src/components/shared/answersetView/resultsTable/Table.jsx
+++ src/components/shared/answersetView/resultsTable/Table.jsx
@@ -1,5 +1,5 @@
 /* eslint-disable react/jsx-props-no-spreading */
-import React, { useState } from 'react';
+import React from 'react';
 import {
   useTable, usePagination, useExpanded, useSortBy, useFilters,
 } from 'react-table';
