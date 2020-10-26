b4cdcae199da12b46f7073ad83f8227ce374b060
Alon Greyber <alon@greyber.org>
2020-10-26 17:03:56 -0400

    Added argument to manage script to enable hot reloading

--- manage.js
+++ manage.js
@@ -5,9 +5,13 @@ import { hideBin } from 'yargs/helpers'
 import colors from 'colors';
 
 function runCommand(cmd) {
+	// Strip newlines so we can use multiline templates
+	cmd = cmd.replace(/(\r\n|\n|\r)/gm, "");
 	console.log(colors.green(cmd));
 	execSync(cmd, { stdio: 'inherit' });
 }
+
+let baseCommand = 'docker-compose -f docker-compose.base.yml ';
 
 yargs(hideBin(process.argv))
   .scriptName('manage')
@@ -15,16 +19,29 @@ yargs(hideBin(process.argv))
   .command({
     command: 'dev',
     describe: `This command starts up a development environment. The development environment is started through docker-compose and is visible at http://localhost.`,
-    handler: (parsed) => {
-		runCommand('docker-compose -f docker-compose.base.yml -f docker-compose.dev.yml up --build --renew-anon-volumes --abort-on-container-exit');
+	builder: {
+	  hotReload: {
+		  type: 'boolean',
+		  default: false,
+	  }
+	},
+    handler: (argv) => {
+		let envs = "";
+		if(argv.hotReload) {
+			envs = "FRONTEND_COMMAND='npm run hot-reload'"
+		}
+		runCommand(`${envs} ${baseCommand} -f docker-compose.dev.yml 
+			up --build --renew-anon-volumes
+			--abort-on-container-exit`);
     },
   })
 
   .command({
     command: 'local-prod',
     describe: `This command starts up a development environment. The development environment is started through docker-compose and is visible at http://localhost.`,
-    handler: (parsed) => {
-		runCommand('docker-compose -f docker-compose.base.yml -f docker-compose.prod.yml up --build --renew-anon-volumes --abort-on-container-exit');
+    handler: (argv) => {
+		runCommand(`${baseCommand} -f docker-compose.prod.yml
+			up --build --renew-anon-volumes --abort-on-container-exit`);
     },
   })
 
