1e5acf66f1659213b0bcbfb786bf50a9d4053682
Alon Greyber <alon@greyber.org>
2020-11-11 15:04:46 -0500

    Switched to JSON errors in express server

--- frontend/queryDispatcher/index.js
+++ frontend/queryDispatcher/index.js
@@ -3,4 +3,11 @@ const routes = require('./routes');
 
 router.use('/api', routes);
 
+const jsonErrorHandler = (req, res) => {
+  res.status(500).send({ status: 'error', message: 'QueryDispatcher Server Error' });
+  res.status(404).send({ status: 'error', message: 'QueryDispatcher Not Found' });
+};
+
+router.use(jsonErrorHandler);
+
 module.exports = router;
