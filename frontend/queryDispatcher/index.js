ef508ea3e06bf881d10078a126c2e843d362f664
Alon Greyber <alon@greyber.org>
2020-12-09 14:33:23 -0500

    Set maxBodyLength and maxContentLength

--- frontend/queryDispatcher/index.js
+++ frontend/queryDispatcher/index.js
@@ -1,5 +1,10 @@
+const axios = require('axios');
 const router = require('express').Router();
 const routes = require('./routes');
+
+// Set axios request max size
+axios.defaults.maxContentLength = Infinity;
+axios.defaults.maxBodyLength = Infinity;
 
 router.use('/api', routes);
 
