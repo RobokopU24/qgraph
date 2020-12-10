defa206492e06060d1677eab8472dac750c60b7b
Alon Greyber <alon@greyber.org>
2020-12-10 13:13:33 -0500

    Split frontend and queryDispatcher services

--- frontend/webpack.dev.js
+++ frontend/webpack.dev.js
@@ -1,7 +1,39 @@
+const path = require('path');
+const webpack = require('webpack');
 const { merge } = require('webpack-merge');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+
 const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
+  devServer: {
+    historyApiFallback: true,
+    disableHostCheck: true,
+    host: '0.0.0.0',
+    port: 80,
+  },
   devtool: 'cheap-module-eval-source-map',
   mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.[jt]sx?$/,
+        exclude: /node_modules/,
+        use: [
+          {
+            loader: require.resolve('babel-loader'),
+            options: {
+              plugins: [
+                require.resolve('react-refresh/babel'),
+              ].filter(Boolean),
+            },
+          },
+        ],
+      },
+    ],
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new ReactRefreshWebpackPlugin(),
+  ],
 });
