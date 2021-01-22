65744aa14f23f8896d053def002ce9c75b558f5c
Max Wang <max@covar.com>
2021-01-22 15:32:43 -0500

    Standardize type array format for incoming nodes and edges

--- frontend/src/pages/question/questionBuilder/QuestionBuilder.jsx
+++ frontend/src/pages/question/questionBuilder/QuestionBuilder.jsx
@@ -77,11 +77,8 @@ export default function QuestionBuilder(props) {
    * @param {Number} question.max_connectivity max connections for question
    */
   function onQuestionTemplate(question) {
-    Object.values(question.query_graph.edges).forEach((e) => {
-      if (e.type && !Array.isArray(e.type)) {
-        e.type = [e.type];
-      }
-    });
+    Object.values(question.query_graph.edges).forEach(queryGraphUtils.standardizeType);
+    Object.values(question.query_graph.nodes).forEach(queryGraphUtils.standardizeType);
     questionStore.updateQueryGraph(question.query_graph);
     questionStore.updateQuestionName(question.natural_question);
     setStep('build');
@@ -119,13 +116,10 @@ export default function QuestionBuilder(props) {
       if (!node.label) {
         node.label = `${node.id}: ${node.name || node.curie || node.type}`;
       }
+      queryGraphUtils.standardizeType(node);
     });
 
-    fileContentObj.query_graph.edges.forEach((e) => {
-      if (e.type && !Array.isArray(e.type)) {
-        e.type = [e.type];
-      }
-    });
+    fileContentObj.query_graph.edges.forEach(queryGraphUtils.standardizeType);
 
     questionStore.updateQueryGraph(
       queryGraphUtils.convert.reasonerToInternal(fileContentObj.query_graph),
