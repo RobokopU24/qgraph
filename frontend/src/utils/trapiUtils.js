eb05974c15b4ee1b951d3da595d67dc26b399680
Max Wang <max@covar.com>
2021-02-02 14:09:44 -0500

    Update trapiUtils error reporting

--- frontend/src/utils/trapiUtils.js
+++ frontend/src/utils/trapiUtils.js
@@ -10,64 +10,75 @@
  * Simple graph validity checks
  * Returns list of errors
 */
-function validateGraph(graph) {
+function validateGraph(graph, graphType) {
   const errors = [];
 
   if (!graph || graph.constructor !== Object) {
-    errors.push('Graph is not a valid Javascript object');
+    errors.push(`${graphType} is not a valid JSON object`);
     return errors;
   }
 
   // Check for nodes
   if (!graph.nodes) {
-    errors.push('A query graph requires a "nodes" property.');
+    errors.push(`${graphType} requires a "nodes" property`);
+  } else if (Array.isArray(graph.nodes)) {
+    errors.push(`${graphType} nodes should be an object`);
   } else {
-    if (Array.isArray(graph.nodes)) {
-      errors.push('Nodes should be an object.');
-    }
     // Since every node has an id we can check if they are unique
     const nodeIds = new Set(Object.keys(graph.nodes));
     const hasUniqueNodeIds = nodeIds.size === Object.keys(graph.nodes).length;
     if (!hasUniqueNodeIds) {
-      errors.push('There are multiple nodes with the same ID.');
+      errors.push(`There are multiple ${graphType.toLowerCase()} nodes with the same ID`);
     }
   }
 
   // Check for edges
   if (!graph.edges) {
-    errors.push('A query graph requires an "edges" property.');
+    errors.push(`${graphType} requires an "edges" property`);
+  } else if (Array.isArray(graph.edges)) {
+    errors.push(`${graphType} edges should be an object`);
   } else {
-    if (Array.isArray(graph.edges)) {
-      errors.push('Edges should be an object.');
-    }
     // each edge should have a valid source and target id
     const edgesHaveIds = Object.keys(graph.edges).reduce((val, e) => val && graph.edges[e] && graph.edges[e].subject && graph.edges[e].object, true);
     if (!edgesHaveIds) {
-      errors.push('Each edge must have a valid "subject" and a "object" property.');
+      errors.push(`Each ${graphType.toLowerCase()} edge must have a valid "subject" and a "object" property`);
     }
   }
 
   return errors;
 }
 
-/*
- * Validate a single instance of a result
-*/
-function validateResult(r) {
-  if (!('node_bindings' in r)) {
-    return 'No node_bindings in result object';
-  }
-  if (!('edge_bindings' in r)) {
-    return 'No edge_bindings in result object';
+/**
+ * Validate message results
+ * Return a list of errors as soon as we find one
+ * malformed result
+ */
+function validateResults(results) {
+  const errors = [];
+
+  if (!Array.isArray(results)) {
+    errors.push('Message results should be an array');
+    return errors;
   }
 
-  if (!Array.isArray(r.node_bindings)) {
-    return 'Node bindings must be array';
+  for (let i = 0; i < results.length; i += 1) {
+    if (!('node_bindings' in results[i])) {
+      errors.push('No node_bindings in result object');
+    } else if (!Array.isArray(results[i].node_bindings)) {
+      errors.push('Node bindings must be array');
+    }
+
+    if (!('edge_bindings' in results[i])) {
+      errors.push('No edge_bindings in result object');
+    } else if (!Array.isArray(results[i].edge_bindings)) {
+      errors.push('Edge bindings must be array');
+    }
+    if (errors.length) {
+      break;
+    }
   }
-  if (!Array.isArray(r.edge_bindings)) {
-    return 'Node bindings must be array';
-  }
-  return null;
+
+  return errors;
 }
 
 /**
@@ -77,25 +88,17 @@ function validateResult(r) {
 function validateMessage(message) {
   const errors = [];
   if (!message || message.constructor !== Object) {
-    errors.push('The uploaded message isn\'t a valid JSON object.');
+    errors.push('The uploaded message isn\'t a valid JSON object');
     return errors;
   }
   if (!('message' in message)) {
-    errors.push('The uploaded message should follow the TRAPI Standard format.');
+    errors.push('The uploaded message should follow the TRAPI Standard format');
     return errors;
   }
 
-  errors.concat(validateGraph(message.message.knowledge_graph));
-  errors.concat(validateGraph(message.message.query_graph));
-
-  if (!Array.isArray(message.message.results)) {
-    errors.push('Message results should be an array.');
-    return errors;
-  }
-
-  const resultErrors = message.message.results.map(validateResult);
-  // Add results errors if not null
-  errors.concat(resultErrors.flat().filter((e) => !!e));
+  errors.concat(validateGraph(message.message.knowledge_graph, 'Knowledge Graph'));
+  errors.concat(validateGraph(message.message.query_graph, 'Query Graph'));
+  errors.concat(validateResults(message.message.results));
 
   return errors;
 }
