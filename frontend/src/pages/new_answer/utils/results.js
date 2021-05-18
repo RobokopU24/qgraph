3b1aaabf3602dec9439a60e4aea4a2cce55619ba
Max Wang <max@covar.com>
2021-05-18 14:33:34 -0400

    Add documentation to results functions

--- frontend/src/pages/new_answer/utils/results.js
+++ frontend/src/pages/new_answer/utils/results.js
@@ -2,14 +2,24 @@ import stringUtils from '~/utils/strings';
 import queryGraphUtils from '~/utils/queryGraph';
 import queryBuilderUtils from '~/utils/queryBuilder';
 
-function findStartingNode(q_graph) {
-  const nodes = Object.entries(q_graph.nodes).map(([key, node]) => (
+/**
+ * Find the first node id for topological sorting
+ * @param {object} query_graph - query graph object
+ *
+ * *Rules:*
+ * 1. Sort by pinned and then unpinned nodes
+ * 2. Sort by number of connected edges
+ * 3. Pick first node in insertion order
+ * @returns {string} the starting node id
+ */
+function findStartingNode(query_graph) {
+  const nodes = Object.entries(query_graph.nodes).map(([key, node]) => (
     {
       key,
       pinned: node.id && Array.isArray(node.id) && node.id.length > 0,
     }
   ));
-  const edgeNums = queryBuilderUtils.getNumEdgesPerNode(q_graph);
+  const edgeNums = queryBuilderUtils.getNumEdgesPerNode(query_graph);
   const unpinnedNodes = nodes.filter((node) => !node.pinned && node.key in edgeNums);
   const pinnedNodes = nodes.filter((node) => node.pinned && node.key in edgeNums);
   let startingNode = (nodes.length && nodes[0].key) || null;
@@ -25,6 +35,10 @@ function findStartingNode(q_graph) {
 
 /**
  * Find the directly connected nodes
+ * @param {object} edges - edges from a query graph
+ * @param {array} nodeList - list of node ids
+ *
+ * **Modifies the nodeList arg**
  */
 function findConnectedNodes(edges, nodeList) {
   const nodeId = nodeList[nodeList.length - 1];
@@ -49,23 +63,34 @@ function findConnectedNodes(edges, nodeList) {
 
 /**
  * Sort nodes for results table headers
- * @param {object} query_graph
+ * @param {object} query_graph - query graph object
+ * @param {string} startingNode - node id
  * @returns {string[]} topologically sorted nodes
  */
 function sortNodes(query_graph, startingNode) {
   const sortedNodes = [startingNode];
   findConnectedNodes(query_graph.edges, sortedNodes);
-  // TODO: handle detached sub-graphs
+  // TODO: handle detached sub-graphs, right now those nodes will be tacked on the end in insertion order
   // include any detached nodes at the end
   const extraNodes = Object.keys(query_graph.nodes).filter((nodeId) => sortedNodes.indexOf(nodeId) === -1);
   return [...sortedNodes, ...extraNodes];
 }
 
+/**
+ * Make a list of table headers for the result table
+ * @param {object} message - full TRAPI message
+ * @param {function} colorMap - function to get color from node category
+ * @returns {object[]} list of table header objects
+ */
 function makeTableHeaders(message, colorMap) {
   const { query_graph, knowledge_graph } = message;
   // startingNode could be undefined for fully cyclic graph
   // topologically sort query graph nodes
   const startingNode = findStartingNode(query_graph);
+  if (!startingNode) {
+    // If there are no query graph nodes, don't show the results table
+    return [];
+  }
   const sortedNodes = sortNodes(query_graph, startingNode);
   const headerColumns = sortedNodes.map((id) => {
     const qgNode = query_graph.nodes[id];
