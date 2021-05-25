71c5c5c3566e3da2600664d0c5ba9e874c6bd5d7
Max Wang <max@covar.com>
2021-05-25 13:54:02 -0400

    Update question library page to navigate to new answer page

--- frontend/src/pages/questionList/subComponents/QuestionTableRow.jsx
+++ frontend/src/pages/questionList/subComponents/QuestionTableRow.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { useHistory } from 'react-router-dom';
 
 import TableRow from '@material-ui/core/TableRow';
@@ -9,16 +9,39 @@ import ClearIcon from '@material-ui/icons/Clear';
 
 import { green, red } from '@material-ui/core/colors';
 
+import API from '~/API';
 import { useVisibility, formatDateTimeNicely } from '~/utils/cache';
+import UserContext from '~/context/user';
 
 export default function QuestionTableRow({ question }) {
   const history = useHistory();
   const visibility = useVisibility();
+  const user = useContext(UserContext);
+
+  async function getAnswers() {
+    let token;
+    if (user) {
+      token = user.id_token;
+    }
+    const response = await API.cache.getAnswersByQuestion(question.id, token);
+    if (response.status === 'error') {
+      console.log('failed to get answers');
+      // pageStatus.setFailure(response.message);
+      return;
+    }
+
+    if (response.length) {
+      const answerId = response[0].id;
+      history.push(`/answer/${answerId}`);
+    } else {
+      console.log('There are no answers for this question');
+    }
+  }
 
   return (
     <TableRow
       key={question.id}
-      onClick={() => history.push(`/question/${question.id}`)}
+      onClick={getAnswers}
       hover
       style={{ cursor: 'pointer' }}
     >
