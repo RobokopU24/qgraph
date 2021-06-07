0acc1463b60388973064222b4180ed212a4c7faf
Max Wang <max@covar.com>
2021-06-07 14:18:05 -0400

    Add message when no support publications for result

--- frontend/src/pages/answer/resultsTable/SupportingPublications.jsx
+++ frontend/src/pages/answer/resultsTable/SupportingPublications.jsx
@@ -1,4 +1,4 @@
-import React, { useReducer, useEffect } from 'react';
+import React, { useReducer, useEffect, useMemo } from 'react';
 import Paper from '@material-ui/core/Paper';
 import List from '@material-ui/core/List';
 import ListItem from '@material-ui/core/ListItem';
@@ -33,48 +33,58 @@ export default function SupportingPublications({ metaData }) {
     dispatch({ type: 'clear' });
   }, [metaData]);
 
+  const hasSupportPublications = useMemo(() => !!Object.values(metaData).find((edge) => edge.length), [metaData]);
+
   return (
     <Paper
       id="resultMetaData"
       elevation={3}
     >
       <h4>Supporting Publications</h4>
-      <List>
-        {Object.entries(metaData).map(([edgeDescription, publications]) => (
-          <React.Fragment key={shortid.generate()}>
-            {publications.length > 0 && (
-              <>
-                <ListItem
-                  button
-                  onClick={() => dispatch({ type: 'toggle', key: edgeDescription })}
-                >
-                  <ListItemText primary={edgeDescription} />
-                  {open[edgeDescription] ? <ExpandLess /> : <ExpandMore />}
-                </ListItem>
-                <Collapse in={open[edgeDescription]} timeout="auto" unmountOnExit>
-                  <List component="div">
-                    {publications.map((publication) => (
-                      <ListItem
-                        button
-                        component="a"
-                        key={shortid.generate()}
-                        href={`https://www.ncbi.nlm.nih.gov/pubmed/${publication.split(':')[1]}/`}
-                        target="_blank"
-                        rel="noreferrer"
-                      >
-                        <ListItemText
-                          primary={`https://www.ncbi.nlm.nih.gov/pubmed/${publication.split(':')[1]}/`}
-                          inset
-                        />
-                      </ListItem>
-                    ))}
-                  </List>
-                </Collapse>
-              </>
-            )}
-          </React.Fragment>
-        ))}
-      </List>
+      {hasSupportPublications ? (
+        <List>
+          {Object.entries(metaData).map(([edgeDescription, publications]) => (
+            <React.Fragment key={shortid.generate()}>
+              {publications.length > 0 && (
+                <>
+                  <ListItem
+                    button
+                    onClick={() => dispatch({ type: 'toggle', key: edgeDescription })}
+                  >
+                    <ListItemText primary={edgeDescription} />
+                    {open[edgeDescription] ? <ExpandLess /> : <ExpandMore />}
+                  </ListItem>
+                  <Collapse in={open[edgeDescription]} timeout="auto" unmountOnExit>
+                    <List component="div">
+                      {publications.map((publication) => (
+                        <ListItem
+                          button
+                          component="a"
+                          key={shortid.generate()}
+                          href={`https://www.ncbi.nlm.nih.gov/pubmed/${publication.split(':')[1]}/`}
+                          target="_blank"
+                          rel="noreferrer"
+                        >
+                          <ListItemText
+                            primary={`https://www.ncbi.nlm.nih.gov/pubmed/${publication.split(':')[1]}/`}
+                            inset
+                          />
+                        </ListItem>
+                      ))}
+                    </List>
+                  </Collapse>
+                </>
+              )}
+            </React.Fragment>
+          ))}
+        </List>
+      ) : (
+        <List>
+          <ListItem>
+            <ListItemText primary="No Supporting Publications Found" />
+          </ListItem>
+        </List>
+      )}
     </Paper>
   );
 }
