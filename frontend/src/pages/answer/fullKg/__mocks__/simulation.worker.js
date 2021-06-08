0e1fb37bdd4814fcc3bc834e8c187eb42d9bcb6a
Max Wang <max@covar.com>
2021-06-08 17:18:56 -0400

    Add more documentation to mock web worker

--- frontend/src/pages/answer/fullKg/__mocks__/simulation.worker.js
+++ frontend/src/pages/answer/fullKg/__mocks__/simulation.worker.js
@@ -1,5 +1,11 @@
 /**
  * Mock Web Worker class for testing
+ *
+ * This is a bare bones web worker that does nothing.
+ * It is needed to test the KgFull component which imports the worker.
+ * As the only thing the web worker does is create a d3 force simulation,
+ * which is a third party package, I (Max Wang) decided that we don't
+ * need to test the returned simulation.
  */
 class Worker {
   constructor(stringUrl) {
