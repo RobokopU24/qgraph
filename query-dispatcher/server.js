830288f314dac4fcb9763ea9ee104b4250785ae6
Max Wang <max@covar.com>
2021-09-14 14:13:04 -0400

    Add logging to query dispatcher

--- query-dispatcher/server.js
+++ query-dispatcher/server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const axios = require('axios');
 const bodyParser = require('body-parser');
+// standard express logger
+const morgan = require('morgan');
 
 const routes = require('./routes');
 
@@ -12,6 +14,7 @@ axios.defaults.maxBodyLength = Infinity;
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json({ limit: '4000mb' }));
+app.use(morgan('common'));
 
 // Add routes
 app.use('/', routes);
