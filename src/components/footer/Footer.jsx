175e0dee304a6e8d2f9da0eab501c09e4ce28d63
David Glymph <Woozl@users.noreply.github.com>
2023-03-23 11:55:01 -0400

    update funding message

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -11,11 +11,12 @@ export default function Footer() {
         <Row>
           <Col md={12}>
             <p>
-              ROBOKOP is a joint creation
-              of <a href="http://www.renci.org">RENCI</a> and <a href="http://www.covar.com">CoVar</a> with
-              funding from the <a href="https://ncats.nih.gov">U.S. NIH NCATS</a> as part
-              of the <a href="https://ncats.nih.gov/translator">Biomedical Data Translator</a>.
-              See our <a href="/termsofservice">Terms of Service</a>.
+              ROBOKOP is a joint creation of <a href="http://www.renci.org" target="_blank">RENCI</a>{" "} 
+              and <a href="http://www.covar.com" target="_blank">CoVar</a>. Early development was 
+              supported by the <a href="https://ncats.nih.gov" target="_blank">NCATS</a> and continued 
+              development is supported by the <a href="niehs.nih.gov" target="_blank">NIEHS</a> and{" "}
+              <a href="https://datascience.nih.gov/about/odss" target="_blank">ODSS</a> within the{" "}
+              <a href="https://www.nih.gov/" target="_blank">NIH</a>. <a href="/termsofservice">Terms of Service</a>.
             </p>
           </Col>
         </Row>
