62eefa668bb97342100953c93dc553a3e9cf5d22
David Glymph <Woozl@users.noreply.github.com>
2023-03-24 15:13:14 -0400

    update funding text for readability/accuracy

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -12,11 +12,12 @@ export default function Footer() {
           <Col md={12}>
             <p>
               ROBOKOP is a joint creation of <a href="http://www.renci.org" target="_blank" rel="noreferrer">RENCI</a>{' '}
-              and <a href="http://www.covar.com" target="_blank" rel="noreferrer">CoVar</a>. Early development was{' '}
-              supported by the <a href="https://ncats.nih.gov" target="_blank" rel="noreferrer">NCATS</a> and continued{' '}
-              development is supported by the <a href="niehs.nih.gov" target="_blank" rel="noreferrer">NIEHS</a> and{' '}
-              <a href="https://datascience.nih.gov/about/odss" target="_blank" rel="noreferrer">ODSS</a> within the{' '}
-              <a href="https://www.nih.gov/" target="_blank" rel="noreferrer">NIH</a>. <a href="/termsofservice">Terms of Service</a>.
+              and <a href="http://www.covar.com" target="_blank" rel="noreferrer">CoVar LLC</a>. Early development was{' '}
+              supported by <a href="https://ncats.nih.gov" target="_blank" rel="noreferrer">NCATS</a>; continued{' '}
+              development is supported by <a href="niehs.nih.gov" target="_blank" rel="noreferrer">NIEHS</a> and{' '}
+              the <a href="https://www.nih.gov/" target="_blank" rel="noreferrer">NIH</a>{' '}
+              <a href="https://datascience.nih.gov/about/odss" target="_blank" rel="noreferrer">ODSS</a>.{' '}
+              <a href="/termsofservice">Terms of Service</a>.
             </p>
           </Col>
         </Row>
