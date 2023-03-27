0179c27e5b27458a530a56d721583c96afa97e84
David Glymph <Woozl@users.noreply.github.com>
2023-03-27 10:43:23 -0400

    add popover and table

---
+++ src/pages/answer/resultsTable/AttributesTable.jsx
@@ -0,0 +1,48 @@
+import {
+  styled,
+  Table, TableBody, TableCell, TableHead, TableRow,
+} from '@material-ui/core';
+import React from 'react';
+
+const headerStyles = { fontWeight: 'bold', backgroundColor: '#eee' };
+
+const StyledTableBody = styled(TableBody)(() => ({
+  '& .MuiTableRow-root:last-of-type .MuiTableCell-root': {
+    borderBottom: 'none',
+  },
+}));
+
+const AttributesTable = ({ attributes }) => (
+  <Table size="small" aria-label="edge attributes table">
+    <TableHead>
+      <TableRow>
+        <TableCell style={headerStyles}>
+          attribute_type_id
+        </TableCell>
+        <TableCell style={headerStyles}>
+          value
+        </TableCell>
+      </TableRow>
+    </TableHead>
+    <StyledTableBody>
+      {attributes.map((attribute, index) => (
+        <TableRow key={index}>
+          <TableCell style={{ verticalAlign: 'top' }}>{attribute.attribute_type_id}</TableCell>
+          <TableCell>
+            <ul style={{ padding: 0, margin: 0, listStyleType: 'none' }}>
+              {Array.isArray(attribute.value) ? (
+                attribute.value.map((valueItem, valueItemIndex) => (
+                  <li key={valueItemIndex}>{valueItem}</li>
+                ))
+              ) : (
+                <li>{attribute.value}</li>
+              )}
+            </ul>
+          </TableCell>
+        </TableRow>
+      ))}
+    </StyledTableBody>
+  </Table>
+);
+
+export default AttributesTable;
