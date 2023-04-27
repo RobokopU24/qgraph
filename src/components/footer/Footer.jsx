bea04da37aa7ea3de2955fe81bc15aea58630b5c
David Glymph <Woozl@users.noreply.github.com>
2023-04-27 15:26:47 -0400

    fix niehs link in funding statement

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -9,7 +9,7 @@ export default function Footer() {
         ROBOKOP is a joint creation of <a href="http://www.renci.org" target="_blank" rel="noreferrer">RENCI</a>{' '}
         and <a href="http://www.covar.com" target="_blank" rel="noreferrer">CoVar LLC</a>. Early development was{' '}
         supported by <a href="https://ncats.nih.gov" target="_blank" rel="noreferrer">NCATS</a>; continued{' '}
-        development is supported by <a href="niehs.nih.gov" target="_blank" rel="noreferrer">NIEHS</a> and{' '}
+        development is supported by <a href="https://niehs.nih.gov" target="_blank" rel="noreferrer">NIEHS</a> and{' '}
         the <a href="https://www.nih.gov/" target="_blank" rel="noreferrer">NIH</a>{' '}
         <a href="https://datascience.nih.gov/about/odss" target="_blank" rel="noreferrer">ODSS</a>.{' '}
         <a href="/termsofservice">Terms of Service</a>.
