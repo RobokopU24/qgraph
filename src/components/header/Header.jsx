dfec2df590347be607e78066846f0cdbef868bad
David Glymph <Woozl@users.noreply.github.com>
2023-06-21 11:22:00 -0400

    update links

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -26,13 +26,14 @@ export default function Header() {
   return (
     <AppBar position="relative" className="header">
       <Toolbar id="headerToolbar">
-        <MuiLink href="https://robokop.renci.org" style={{ cursor: 'pointer', margin: 0 }}><Logo height="48px" width="100%" style={{ paddingTop: '6px' }} /></MuiLink>
+        <MuiLink href="/" style={{ cursor: 'pointer', margin: 0 }}><Logo height="48px" width="100%" style={{ paddingTop: '6px' }} /></MuiLink>
         <div className="grow" />
         <Link to="/">Question Builder</Link>
         <Link to="/about">About</Link>
         <Link to="/guide">Guide</Link>
         <Link to="/tutorial">Tutorial</Link>
-        <a href="https://robokop.renci.org/#contact" target="_blank" rel="noreferrer">Help</a>
+        {/* This will go to the actual root of the host (robokop.renci.org/#contact), not an internal route in this application */}
+        <a href="/#contact">Help</a>
         <Divider orientation="vertical" variant="middle" flexItem />
         <IconButton
           onClick={(e) => (
