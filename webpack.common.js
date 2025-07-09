3e89c869830f6fa5f893da3e119a6d089d736597
David Glymph <Woozl@users.noreply.github.com>
2025-07-09 15:21:34 -0400

    Revert "Merge pull request #310 from RobokopU24/bugfix/env-variables-react"
    This reverts commit c2ac5ca77283e586f981efda9f95768e5f685eb4, reversing
    changes made to 5866cf3cc5ac9578b5693b35a7fcb97709a8a967.

--- webpack.common.js
+++ webpack.common.js
@@ -1,14 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const webpack = require('webpack');
-const dotenv = require('dotenv');
-
-const env = dotenv.config().parsed || {};
-
-const envKeys = Object.keys(env).reduce((prev, next) => {
-  prev[`process.env.${next}`] = JSON.stringify(env[next]);
-  return prev;
-}, {});
 
 const config = {
   entry: ['./src/index.jsx'],
@@ -72,7 +64,9 @@ const config = {
       template: './public/index.html',
       favicon: './public/favicon.ico',
     }),
-    new webpack.DefinePlugin(envKeys),
+    new webpack.DefinePlugin({
+      'process.env': JSON.stringify(process.env),
+    }),
   ],
 };
 
